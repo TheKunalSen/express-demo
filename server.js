5e33bf1ac763b05c1c05469c171eb76770943b02
TheKunalSen <ksen959595@gmail.com>
2024-02-20 23:05:14 +0530

    first commit

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 // Importing necessary modules
 const express = require('express');
+const cors = require('cors'); // Import cors module
 
 // Creating an instance of Express
 const app = express();
 const port = 3000; // Port number
-const hostname = '0.0.0.0'
+
+// Use cors middleware
+app.use(cors());
+
 // Define a route that returns a JSON response
 app.get('/api/data', (req, res) => {
     // Sample JSON data
@@ -19,6 +23,6 @@ app.get('/api/data', (req, res) => {
 });
 
 // Start the server
-app.listen(port,hostname, () => {
+app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
